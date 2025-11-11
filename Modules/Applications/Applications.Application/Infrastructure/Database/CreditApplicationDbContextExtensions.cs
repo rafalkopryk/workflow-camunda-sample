1fa65a5daa1e05090633a2baeca6befd89b8ff37
rafal <rafal.kopryk@gmail.com>
2025-11-11 18:31:56 +0100

    Update to .net 10, aspire 13

--- Modules/Applications/Applications.Application/Infrastructure/Database/CreditApplicationDbContextExtensions.cs
+++ Modules/Applications/Applications.Application/Infrastructure/Database/CreditApplicationDbContextExtensions.cs
@@ -6,35 +6,26 @@ namespace Applications.Application.Infrastructure.Database;
 
 internal static class CreditApplicationDbContextExtensions
 {
+    extension(CreditApplicationDbContext dbContext)
+    {
+        public async Task<CreditApplication?> GetCreditApplicationAsync(string applicationId)
+        {
+            return await dbContext.Applications.FirstOrDefaultAsync(x => x.Id == applicationId);
+        }
+        
+        public async Task<bool> HasCreditApplicationAsync(string applicationId)
+        {
+            return (await dbContext.Applications.CountAsync(x => x.Id == applicationId)) > 0;
+        }
+    }
+    
     private static readonly Func<CreditApplicationDbContext, string, Task<CreditApplication?>> GetApplication =
         EF.CompileAsyncQuery(
             (CreditApplicationDbContext dbContext, string applicationId) =>
                 dbContext.Applications.FirstOrDefault(application => application.Id == applicationId));
 
-    public static async Task<CreditApplication?> GetCreditApplicationAsync(this CreditApplicationDbContext dbContext, string applicationId)
-    {
-        try
-        {
-            return await dbContext.Applications.FirstOrDefaultAsync(x => x.Id == applicationId);
-
-        }
-        catch (Exception ex)
-        {
-            throw;
-        }
-
-        //return await GetApplication(dbContext, applicationId);
-    }
-
     private static readonly Func<CreditApplicationDbContext, string, Task<bool>> HasApplication =
         EF.CompileAsyncQuery(
             (CreditApplicationDbContext dbContext, string applicationId) =>
                 dbContext.Applications.Any(application => application.Id == applicationId));
-
-    public static async Task<bool> HasCreditApplicationAsync(this CreditApplicationDbContext dbContext, string applicationId)
-    {
-        return (await dbContext.Applications.CountAsync(x => x.Id == applicationId)) > 0;
-
-        //return await HasApplication(dbContext, applicationId);
-    }
 }
--- external/camunda-startup/CamundaStartup.Aspire.Hosting.Camunda/CamundaContainerImageTags.cs
+++ external/camunda-startup/CamundaStartup.Aspire.Hosting.Camunda/CamundaContainerImageTags.cs
@@ -4,5 +4,5 @@ internal static class CamundaContainerImageTags
 {
     public const string Image = "camunda/camunda";
 
-    public const string Tag = "8.8.0";
+    public const string Tag = "8.8.3";
 }
